ae183adc7372d791afc01980b29ff752196100e3
RobertineIFC <76454392+RobertineIFC@users.noreply.github.com>
2023-03-08 13:26:14 +0100

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,15 @@
-function search_topic() {
-	let input = document.getElementById('searchbar').value
-	input = input.toLowerCase();
-	let x = document.getElementsByClassName('dropdown-content');
 
-	for (i = 0; i < x.length; i++) {
-		if (!x[i].innerHTML.toLowerCase().includes(input)) {
-			x[i].style.display = "none";
-		} else {
-			x[i].style.display = "table-row";
-		}
-	}
+function search_topic() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('units');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
 }
